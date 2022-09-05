86f628edb3997bc3230ad4faf34a12cdd1ae353f
abdoul-lelou <abdourahmane.benzy@gmail.com>
2022-09-05 11:37:55 +0000

    last commit

--- index.js
+++ index.js
@@ -43,3 +43,12 @@ function slideFooter(){
     }
   }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
